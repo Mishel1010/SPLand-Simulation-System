5cff293fc2d9f631d8b9e036795ade10f69517ca
adish-glitch <dyshm4349@gmail.com>
2024-11-19 20:05:56 +0000

    yagsh

--- src/Auxiliary.cpp
+++ src/Auxiliary.cpp
@@ -15,7 +15,6 @@ std::vector<std::string> Auxiliary::parseArguments(const std::string& line) {
     while (stream >> argument) {
         arguments.push_back(argument);
     }
+    std::cout<<"Arguments: ";
     return arguments;
 }
-
-
