f6a87c46cd93b999a960d6d6d874aa35f8c01e57
Mishel Koren <koren3684@gmail.com>
2024-11-19 20:09:48 +0000

    dkjfhs

--- src/Auxiliary.cpp
+++ src/Auxiliary.cpp
@@ -15,13 +15,7 @@ std::vector<std::string> Auxiliary::parseArguments(const std::string& line) {
     while (stream >> argument) {
         arguments.push_back(argument);
     }
-<<<<<<< HEAD
     std::cout<<"Arguments: ";
+    std::cout<<arguments[0]<<std::endl;
     return arguments;
 }
-=======
-    std::cout << "Arguments: ";
-    std::cout << "Arguments2: ";
-    return arguments;
-}
->>>>>>> 1736a1ce9992d477409965dd33bbf5dcf87a9dc4
