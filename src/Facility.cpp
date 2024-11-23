46b6ade5b6f20a4be52306b45eb576123ace6a15
adish-glitch <dyshm4349@gmail.com>
2024-11-23 17:16:04 +0000

    1234

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -55,14 +55,18 @@ Facility::Facility(
     const int economy_score,
     const int environment_score)
     : FacilityType(name, category, price, lifeQuality_score, economy_score, environment_score),
-      settlementName(settlementName){}
+      settlementName(settlementName),
+      status(FacilityStatus::UNDER_CONSTRUCTIONS),
+      timeLeft(price){}
 
-//A constructor that recieves a facillity type        
+       
 Facility::Facility(   
         FacilityType &type, 
         const string &settlementName)
         : FacilityType(type),
-          settlementName(settlementName){}
+          settlementName(settlementName),
+          status(FacilityStatus::UNDER_CONSTRUCTIONS),
+          timeLeft(type.getCost()) {}
 
 const string &Facility::getSettlementName() const
 {
@@ -76,8 +80,11 @@ const int Facility::getTimeLeft() const
         
 FacilityStatus Facility::step()
 {
-    throw "Not implemented";
+    if (timeLeft==1){
+        status = FacilityStatus::OPERATIONAL;}
+    timeLeft--;
 }
+
 
 void Facility::setStatus(FacilityStatus status)
 {
@@ -91,7 +98,8 @@ const FacilityStatus& Facility::getStatus() const
         
 const string Facility::toString() const
 {
-    throw "Not implemented";
+    return  "settlement: " + getSettlementName() + ", faciility: " + getName() + ", price: " + std::to_string(getCost()) + ", time left: " + std::to_string(getTimeLeft()) + ", life quality score: " + std::to_string(getLifeQualityScore())
+    + ", economy score: " + std::to_string(getEconomyScore()) + ", environment score: " + std::to_string(getEnvironmentScore());
 }
         
 
