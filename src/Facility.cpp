29592d83602086fb094d1f9698377ccaf15979f3
Mishel Koren <koren3684@gmail.com>
2024-11-23 15:19:57 +0000

    Facility Changes

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -1,122 +1,99 @@
 #include "Facility.h"
+#include <string>
+#include <iostream>
 
-class Facilitytype {
+FacilityType::FacilityType(
+    const string &name,
+    const FacilityCategory category,
+    const int price,
+    const int lifeQuality_score,
+    const int economy_score,
+    const int environment_score)
+    : name(name),
+      category(category),
+      price(price),
+      lifeQuality_score(lifeQuality_score),
+      economy_score(economy_score),
+      environment_score(environment_score){}
+     
+const string &FacilityType::getName() const
+{
+    return name;
+}
 
+int FacilityType::getCost() const
+{
+    return price;
+}
 
+int FacilityType::getLifeQualityScore() const
+{
+    return lifeQuality_score;
+}
 
-     public: 
-
-    //default constructor
-       Facilitytype()
-       : name("default"), category(FacilityCategory::ECONOMY),price(0), lifeQuality_score(0), economy_score(0), environment_score(0){};
-
-     //constructor
-     Facilitytype
-       (const string& nameInput,
-       FacilityCategory inputcategory,
-       int priceInput,
-       int lifeinput,
-       int ecoinput,
-       int envinput)
-        : name(nameInput), category(inputcategory), price(priceInput), lifeQuality_score(lifeinput), economy_score(ecoinput), environment_score(envinput){}
-        
-
-
-     const string &getName() const{
-      return name;}
-
-     int getCost() const{
-        return price;}
-
-     int getLifeQualityScore() const{
-        return lifeQuality_score;
-     }
-
-     int getEnvironmentScore() const{
+int FacilityType::getEnvironmentScore() const
+{
         return economy_score;
-     }
-     int getEconomyScore() const{
+}
+
+int FacilityType::getEconomyScore() const
+{
         return economy_score;
-     }
+}
 
-     FacilityCategory getCategory() const{
-        return category;
-     }
+FacilityCategory FacilityType::getCategory() const
+{
+    return category;
+}
+
+Facility::Facility(
+    const string &name,
+    const string &settlementName,
+    const FacilityCategory category,
+    const int price,
+    const int lifeQuality_score,
+    const int economy_score,
+    const int environment_score)
+    : FacilityType(name, category, price, lifeQuality_score, economy_score, environment_score),
+      settlementName(settlementName){}
+
+//A constructor that recieves a facillity type        
+Facility::Facility(   
+        FacilityType &type,
+        const string &settlementName)
+        : FacilityType(type),
+          settlementName(settlementName){}
+
+const string &Facility::getSettlementName() const
+{
+    return this->settlementName;
+}
      
-
-protected:
-
-//members
-     const string name;
-     const FacilityCategory category;
-     const int price; 
-     const int lifeQuality_score;
-     const int economy_score;
-     const int environment_score;
-
-
-
-
-
-class Facility: public FacilityType {
-
-    
-
-
-    //constructor
-    Facility(
-        const string &nameinput,
-        const string &settnameinput,
-        const FacilityCategory category,
-        const int priceinput,
-        const int lifescore,
-        const int ecoscore,
-        const int envscore)
-        : settlementName(settnameinput), type(nameinput, category, priceinput, lifescore, ecoscore, envscore), timeLeft(priceinput), status(status::UNDER_CONSTRUCTIONS){}
+const int Facility::getTimeLeft() const
+{
+    return timeLeft;
+}
         
-    Facility(
-        //a constructor that recieves a facillity type
-            const FacilityType &typeinput,
-            const string &settnameinput)
-            : settlementName(settnameinput), type(typeinput), timeLeft(typeinput.getCost()), status(STATUS::UNDER_CONSTRUCTIONS){}
+FacilityStatus Facility::step()
+{
+    throw "Not implemented";
+}
 
-            
-            
+void Facility::setStatus(FacilityStatus status)
+{
+    status=status;
+}
 
-             const string &getSettlementName() const{
-             return settlementname;}
-
-     
-             const string &gettype const{
-                return type.getname();
-             }
-     
-        const int getTimeLeft() const{
-            return timeLeft;}
+const FacilityStatus& Facility::getStatus() const
+{
+    return status;
+}
         
-        FacilityStatus step(){
-            if getTimeLeft()==1{
-               setStatus(OPERATIONAL)}
-            timeLeft--;}
-
-        void setStatus(FacilityStatus status){
-            this.status=status;}
-
-        const FacilityStatus& getStatus() const{
-            return status;}
+const string Facility::toString() const
+{
+    throw "Not implemented";
+}
         
-        const string toString() const{
-            return ""+ gettype + ", " + getsettlementname() + ", time left: " + getTimeLeft() + ", life quality score: " + type.getLifeQualityScore() + ", ecoomy score: " +type.getEconomyScore() + ", environment score: " + type.getEnvironmentScore()
-        }
-    
-     private:
-    //members
-        const string settlementName;
-        FacilityStatus status;
-        int timeLeft;
-        const FacilityType type;
-
-    }
 
 
 
