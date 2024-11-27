fbfe8d4302d34bef572890ced220322548f4d2f5
Mishel Koren <koren3684@gmail.com>
2024-11-27 17:36:58 +0000

    selection and plan

--- src/Plan.cpp
+++ src/Plan.cpp
@@ -1,2 +1,50 @@
 #include "Plan.h"
-// test
+
+ Plan::Plan(const int planId, const Settlement &settlement, SelectionPolicy *selectionPolicy, const vector<FacilityType> &facilityOptions)
+ : plan_id(planId),
+  settlement(settlement),
+  selectionPolicy(selectionPolicy),
+  status(PlanStatus::AVALIABLE),
+  facilities(vector<Facility*>()), 
+  underConstruction(vector<Facility*>()),
+  facilityOptions(facilityOptions),
+  life_quality_score(0), economy_score(0), environment_score(0){}
+
+
+  const int Plan::getlifeQualityScore() const{
+    return life_quality_score;
+  }
+  const int Plan::getEconomyScore() const{
+            return economy_score;
+        }
+  const int Plan::getEnvironmentScore() const{
+            return environment_score;
+        }
+
+    void Plan::setSelectionPolicy(SelectionPolicy *selectionPolicy){
+        this->selectionPolicy=selectionPolicy;
+    }
+  
+  void Plan::step(){
+    if (status == PlanStatus::AVALIABLE){
+      FacilityType Faciltype = selectionPolicy->selectFacility(facilityOptions);
+      Facility facil = Facility(facil, settlement);
+
+      
+    for (int i=0; i<underConstruction.size();){
+      Facility* ptr = underConstruction[i];
+      if (ptr->step() == FacilityStatus::OPERATIONAL){
+        facilities.push_back(ptr);
+        underConstruction.erase(underConstruction.begin()+i);
+        life_quality_score += ptr->getLifeQualityScore();
+        economy_score += ptr->getEconomyScore();
+        environment_score += ptr->getEnvironmentScore();
+      }
+      else {i++;}
+    }
+    if ((settlement.getType() == SettlementType::VILLAGE && underConstruction.size() == 1 ) ||
+     (settlement.getType() == SettlementType::CITY && underConstruction.size() == 2) || 
+      (settlement.getType() == SettlementType::METROPOLIS && underConstruction.size() == 3)){
+        status = PlanStatus::BUSY;
+      }
+  }
--- src/SelectionPolicy.cpp
+++ src/SelectionPolicy.cpp
@@ -27,13 +27,13 @@ BalancedSelection::BalancedSelection(int LifeQualityScore, int EconomyScore, int
 : LifeQualityScore(LifeQualityScore), EconomyScore (EconomyScore), EnvironmentScore(EnvironmentScore){}
 
  const FacilityType& BalancedSelection::selectFacility(const std::vector<FacilityType>& facilitiesOptions){
-    FacilityType* facil = facilitiesOptions[0]*;
-    int a = facil.getEconomyScore()+EconomyScore;
-    int b = facil.getLifeQualityScore()+LifeQualityScore;
-    int c = facil.getEnvironmentScore()+EnvironmentScore;
+    const FacilityType*  ptr = &facilitiesOptions[0];
+    int a = ptr->getEconomyScore()+EconomyScore;
+    int b = ptr->getLifeQualityScore()+LifeQualityScore;
+    int c = ptr->getEnvironmentScore()+EnvironmentScore;
     int sum =std::abs(a-b) + std::abs(b-c) + std::abs(a-c) ;
 
-    for (FacilityType v:facilitiesOptions){
+    for (const FacilityType& v:facilitiesOptions){
         int a = LifeQualityScore + v.getLifeQualityScore();
         int b = v.getEconomyScore()+EconomyScore;
         int c = v.getEnvironmentScore()+EnvironmentScore;
@@ -42,7 +42,51 @@ BalancedSelection::BalancedSelection(int LifeQualityScore, int EconomyScore, int
             return v;
         }
         if (temp < sum){
-            facil = v;}
-            return v;}
+            ptr = &v;}}
+            return *ptr;
 
     }
+
+    EconomySelection::EconomySelection()
+    :lastSelectedIndex(-1){}
+
+    const FacilityType& EconomySelection::selectFacility(const vector<FacilityType>& facilitiesOptions){
+        lastSelectedIndex = (lastSelectedIndex+1)%facilitiesOptions.size();
+        while (facilitiesOptions[lastSelectedIndex].getCategory() != FacilityCategory::ECONOMY){
+          lastSelectedIndex = (lastSelectedIndex+1)%facilitiesOptions.size();  
+        }
+        return facilitiesOptions[lastSelectedIndex];
+        }
+
+    const string EconomySelection::toString() const{
+        return "Economy selection";
+    }
+
+
+     EconomySelection EconomySelection::*clone() {
+
+
+     }
+
+
+     EconomySelection::~EconomySelection(){}
+
+    SustainabilitySelection::SustainabilitySelection()
+    : lastSelectedIndex(-1){}
+
+    const FacilityType& SustainabilitySelection::selectFacility(const vector<FacilityType>& facilitiesOptions){
+        lastSelectedIndex = (lastSelectedIndex+1)%facilitiesOptions.size();
+        while (facilitiesOptions[lastSelectedIndex].getCategory() != FacilityCategory::ENVIRONMENT){
+          lastSelectedIndex = (lastSelectedIndex+1)%facilitiesOptions.size();  
+        }
+        return facilitiesOptions[lastSelectedIndex];
+        }
+
+        const string SustainabilitySelection::toString() const{
+            return "SustainabilitySelection";
+        }
+
+        SustainabilitySelection SustainabilitySelection::*clone() const{
+            
+        }
+        SustainabilitySelection:: ~SustainabilitySelection(){}
